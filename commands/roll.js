fbb5618c4733ddaa03fca5c5173247c895b5c887
John Nguyen <j4nguyen134@gmail.com>
2024-12-20 16:12:39 -0600

    working roll command

---
+++ commands/roll.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("roll")
+    .setDescription("Rolls a dice."),
+
+  async execute(interaction) {
+    var diceImages = ["https://static.thenounproject.com/png/1194688-200.png","https://static.thenounproject.com/png/1194691-200.png","https://static.thenounproject.com/png/1194684-200.png",
+        "https://static.thenounproject.com/png/1194689-200.png","https://static.thenounproject.com/png/1194690-200.png", "https://static.thenounproject.com/png/1194690-200.png"];
+    
+    var selected = diceImages[Math.floor(Math.random()*diceImages.length)];
+    await interaction.reply("https://tenor.com/view/dice-roll-dice-roll-the-dice-cube-game-gif-6495545147822062773");
+    setTimeout(async () => {
+        await interaction.editReply(selected);
+      }, 2000); // 2000ms = 2 seconds
+  },
+};
---
+++ index.js
@@ -0,0 +1,89 @@
+const fs = require("node:fs");
+const {Client, Events, GatewayIntentBits, Collection} = require('discord.js');
+const {token, clientId, guildId} = require('./config.json');
+const {Routes} = require('discord.js');
+const { REST } = require('@discordjs/rest')
+
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]});
+
+client.commands = getCommands("./commands");
+
+client.once(Events.ClientReady, (c) => {
+    registerCommands();
+    console.log(`${c.user.tag} is online.`);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+    if(!interaction.isChatInputCommand()) {
+        return;
+    }
+
+    let command = client.commands.get(interaction.commandName);
+
+    try {
+        if(interaction.replied) return;
+        command.execute(interaction);
+    } catch(error) {
+        console.error(error);
+    }
+});
+
+client.login(token);
+
+function getCommands(dir) {
+    let commands = new Collection();
+    const commandFiles = getFiles(dir);
+
+    for(const commandFile of commandFiles) {
+        const command = require(commandFile);
+        commands.set(command.data.toJSON().name, command);
+    }
+
+    return commands
+}
+
+function getFiles(dir) {
+    const files = fs.readdirSync(dir, {
+        withFileTypes: true
+    });
+    let commandFiles = [];
+
+
+    for (const file of files) {
+        if(file.isDirectory()) {
+            // recursive call getFiles to subdirs
+            commandFiles = [
+                ...commandFiles,
+                ...getFiles(`${dir}/${file.name}`)
+            ];
+        } 
+        // pushes file to commandFiles array
+        else if (file.name.endsWith(".js")) {
+            commandFiles.push(`${dir}/${file.name}`);
+        }
+    }
+
+    return commandFiles;
+}
+
+
+// Register commands
+async function registerCommands() {
+    let commands = [];
+    const commandFiles = getFiles("./commands");
+
+    for (const file of commandFiles) {
+        const command = require(file);
+        commands.push(command.data.toJSON());
+    }
+
+    const rest = new REST({ version: "10" }).setToken(token);
+
+    try {
+        await rest.put(Routes.applicationCommands(clientId), { body: commands });
+        console.log("Successfully deployed commands!");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
